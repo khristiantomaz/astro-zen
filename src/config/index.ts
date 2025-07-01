5ad07de791195e6302f50153465ad9a9bd089bca
khristian tomaz <khristiantomaz@icloud.com>
2025-06-30 22:51:41 -0300

    Portifolio update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,23 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Khristian Tomaz, Software Engineer`",
+  author: "Khristian Tomaz",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-  lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+    "Engenharia de Software, apaixonado por tecnologia e desenvolvimento de software. Sempre buscando aprender e evoluir.",
+  lang: "pt-BR",
+  siteLogo: "/foto-de-perfil.jpg",
   navLinks: [
-    { text: "Experience", href: "#experience" },
-    { text: "Projects", href: "#projects" },
-    { text: "About", href: "#about" },
+    { text: "Diplomas", href: "#experience" },
+    { text: "Projetos", href: "#projects" },
+    { text: "Sobre", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "X", href: "https://x.com/ilussiumv" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/khristian-tomaz/" },
+    { text: "Github", href: "https://github.com/khristiantomaz" },
+    { text: "Instagram", href: "https://www.instagram.com/khris.Tomaz/" },
+    
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,32 +25,33 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
-    summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+    name: "Khristian Tomaz",
+    specialty: "Software Engineer",
+    summary: `Sou apaixonado por tecnologia e sempre busquei aprender cada vez mais, seja lendo, praticando ou explorando novas ferramentas. Esse interesse me levou a ingressar no curso de Engenharia de Software, no qual atualmente curso o sexto semestre.
+Tenho como objetivo evoluir constantemente, tanto na qualidade do meu código quanto na resolução de problemas. Estou sempre em busca de novos desafios que me permitam desenvolver habilidades técnicas e expandir meus conhecimentos.
+Compartilho meus projetos e aprendizados nas redes sociais, como Instagram e X (antigo Twitter). Sinta-se à vontade para acompanhar minha jornada por lá!`,
+
+    email: "Khristiantomaz@icloud.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "Estácio de Sá",
+      position: "Bacharel em Engenharia de Software",
+      startDate: "julho 2022",
+      endDate: "julho 2026",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Hoje me encontro no sexto semestre em Bacharel de engenharia de sofware, construindo um curriculo solido e eficaz para o mercado de trabalho.",
+        "Principais disciplinas: Algoritmos, Estruturas de Dados, Banco de Dados, Engenharia de Software, Desenvolvimento Web, Programação Orientada a Objetos.",
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
+      company: "DIO",
+      position: ".Net Developer",
+      startDate: "Fevereiro 2024",
+      endDate: "Maio 2024",
       summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+        "curso de .Net Developer, onde aprendi a desenvolver aplicações web e desktop utilizando a plataforma .NET, com foco em C# e ASP.NET.",
+        "Principais tópicos abordados: Programação Orientada a Objetos, ASP.NET Core, Entity Framework, Web API, MVC, e testes unitários.",
       ],
     },
     {
@@ -59,7 +60,15 @@ export const SITE_CONTENT: SiteContent = {
       startDate: "Jun 2015",
       endDate: "Oct 2016",
       summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
+        "",
+    },
+    {
+      company: "Driveer",
+      position: "Frontend Developer",
+      startDate: "Jun 2015",
+      endDate: "Oct 2016",
+      summary:
+        "",
     },
   ],
   projects: [
@@ -86,13 +95,11 @@ export const SITE_CONTENT: SiteContent = {
     },
   ],
   about: {
-    description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
-
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+    description: `Atualmente, meu foco é construir uma carreira sólida no mercado de tecnologia, unindo minha paixão por desenvolvimento de software ao desejo constante de evolução. Busco aprender continuamente por meio de cursos, livros e projetos pessoais, sempre com o objetivo de aprimorar minhas habilidades técnicas e ampliar minha visão profissional.
+Estou aberto a novas oportunidades e desafios que me permitam crescer e contribuir com soluções inovadoras. Se você procura um desenvolvedor comprometido, curioso e apaixonado pelo que faz, estou pronto para somar!
     `,
-    image: "/alejandro-big.jpg",
+    image: "/foto-de-perfil.jpg",
   },
 };
 
-// #5755ff
+
